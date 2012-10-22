b3e120711fc9c6cfd2182bc470bf8e403a9545a4
zacc <zcampbell@mozilla.com>
2012-10-22 15:49:20 +0100

    Add mock contact and test contact

---
+++ gaiatest/mocks/mock_contact.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+class MockContact(dict):
+
+    def __init__(self, **kwargs):
+        # set your default values
+        import time
+        self['first_name'] = 'gaia_%s' % repr(time.time()).replace('.', '')
+        self['last_name'] = "test"
+        self['email'] = '%s@restmail.net' % self['name']
+        self['phone_no'] = "07011111111"
+        self['street'] = "101 Testing street"
+        self['zip'] = "90210"
+        self['city'] = "London"
+        self['country'] = "UK"
+        self['comment'] = "Gaia automated test"
+
+        # update with any keyword arguments passed
+        self.update(**kwargs)
+
+    # allow getting items as if they were attributes
+    def __getattr__(self, attr):
+        return self[attr]
--- gaiatest/tests/test_contacts.py
+++ gaiatest/tests/test_contacts.py
@@ -3,18 +3,34 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from gaiatest import GaiaTestCase
-
+from mocks.mock_contact import MockContact
 
 class TestContacts(GaiaTestCase):
 
+    _add_new_contact_button_locator = ('id'', add-contact-button')
+
+    _given_name_field_locator = ('id', 'givenName')
+    _family_name_field_locator = ('id', 'familyName')
+    _email_field_locator = ('id', "email_#i#")
+    _phone_field_locator = ('id', "number_#i#")
+    _street_field_locator = ('id', "streetAddress_#i#")
+    _zip_code_field_locator = ('id', "postalCode_#i#")
+    _city_field_locator = ('id', 'locality_#i#')
+    _country_field_locator = ('id', 'countryName_#i#')
+    _comment_field_locator = ('id', 'note_#i#')
+
+    _done_button_locator = ('id', 'settings-done')
+
     # TODO waiting for bug 800011
-    def incomplete_add_new_contact(self):
+    def test_add_new_contact(self):
         # https://moztrap.mozilla.org/manage/case/1309/
 
         self.assertTrue(self.lockscreen.unlock())
 
+        contact = MockContact()
+
         # launch the Contacts app
-        app = self.apps.launch('communications')
+        app = self.apps.launch('Contacts')
         self.assertTrue(app.frame_id is not None)
 
         # switch into the Contact's frame
@@ -23,36 +39,24 @@ class TestContacts(GaiaTestCase):
         self.assertTrue('communications' in url, 'wrong url: %s' % url)
 
         #click Create new contact
-        self.marionette.find_element('id', 'add-contact-button').click()
-
-        first_name = "NewFirst"
-        last_name = "Contact"
-        phone_no = "07011111111"
-        email = "tester@mozilla.com"
-        street = "101 Testing St"
-        zip = "90210"
-        city = "London"
-        country = "UK"
-        comment = "cool dude"
+        self.marionette.find_element(*self._add_new_contact_button_locator).click()
 
         # Enter data into fields
+        self.marionette.find_element(*self._given_name_field_locator).send_keys(contact['first_name'])
+        self.marionette.find_element(*self._family_name_field_locator).send_keys(contact['last_name'])
+        self.marionette.find_element(*self._phone_field_locator).send_keys(contact['phone_no'])
+        self.marionette.find_element(*self._email_field_locator).send_keys(contact['email'])
 
-        self.marionette.find_element('name', 'givenName').send_keys(first_name)
-        self.marionette.find_element('name', 'familyName').send_keys(last_name)
-        self.marionette.find_element('css selector', "input[placeholder='Phone']").send_keys(phone_no)
-        self.marionette.find_element('css selector', "input[placeholder='Email']").send_keys(email)
+        self.marionette.find_element(*self._street_field_locator).send_keys(street)
+        self.marionette.find_element(*self._zip_code_field_locator).send_keys(contact['zip'])
+        self.marionette.find_element(*self._city_field_locator).send_keys(contact['city'])
+        self.marionette.find_element(*self._country_field_locator).send_keys(contact['country'])
 
-        self.marionette.find_element('css selector', "input[placeholder='Street']").send_keys(street)
-        self.marionette.find_element('css selector', "input[placeholder='Zip code']").send_keys(zip)
-        self.marionette.find_element('css selector', "input[placeholder='City']").send_keys(city)
-        self.marionette.find_element('css selector', "input[placeholder='Country']").send_keys(country)
+        self.marionette.find_element(*self._comment_field_locator).send_keys(contact['comment'])
 
-        self.marionette.find_element('css selector', "input[placeholder='Comment']").send_keys(comment)
-
-        done_button = self.marionette.find_element('id', 'settings-done')
+        done_button = self.marionette.find_element(*self._done_button_locator)
         self.assertTrue(done_button.is_enabled())
         done_button.click()
-
 
         # close the app
         self.apps.kill(app)
