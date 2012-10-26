d140130075eb768d43b0c97496764e009d64f2da
zacc <zcampbell@mozilla.com>
2012-10-26 16:54:38 +0100

    Update to camera test

--- gaiatest/tests/test_camera.py
+++ gaiatest/tests/test_camera.py
@@ -7,7 +7,7 @@ from gaiatest import GaiaTestCase
 
 class TestCamera(GaiaTestCase):
 
-    _capture_photo_locator = ('id', 'capture-button')
+    _capture_button_locator = ('id', 'capture-button')
     _switch_source_button_locator = ('id', 'switch-button')
     _film_strip_image_locator = ('css selector', 'div#film-strip div.image > img')
     _video_timer_locator = ('id', 'video-timer')
@@ -30,8 +30,8 @@ class TestCamera(GaiaTestCase):
     def test_capture_a_photo(self):
         # https://moztrap.mozilla.org/manage/case/1309/
 
-        self.wait_for_element_displayed(*self._capture_photo_locator)
-        self.marionette.find_element(*self._capture_photo_locator).click()
+        self.wait_for_element_displayed(*self._capture_button_locator)
+        self.marionette.find_element(*self._capture_button_locator).click()
 
         self.wait_for_element_present(*self._film_strip_image_locator)
 
@@ -41,18 +41,18 @@ class TestCamera(GaiaTestCase):
 
     def test_capture_a_video(self):
 
-        self.wait_for_element_displayed(*self._capture_photo_locator)
+        self.wait_for_element_displayed(*self._capture_button_locator)
         self.marionette.find_element(*self._switch_source_button_locator).click()
 
        #self.wait_for_element_displayed(*self._video_timer_locator)
 
-        self.marionette.find_element(*self._capture_photo_locator).click()
+        self.marionette.find_element(*self._capture_button_locator).click()
 
         # Wait for 3 seconds of recording
-        self.wait_for_condition(lambda s: self.marionette.find_element(*self._video_timer_locator).text == '00:03')
+        self.wait_for_condition(lambda m: m.find_element(*self._video_timer_locator).text == '00:03')
 
         # Stop recording
-        self.marionette.find_element(*self._capture_photo_locator).click()
+        self.marionette.find_element(*self._capture_button_locator).click()
 
         self.wait_for_element_not_displayed(*self._video_timer_locator)
 
